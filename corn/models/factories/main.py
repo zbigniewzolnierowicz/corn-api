e397d49a7b2a7ef8f148c708820905c29d70825e
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@saucelabs.com>
2023-04-18 01:01:48 +0200

    Add faker

---
+++ corn/models/factories/main.py
@@ -0,0 +1,3 @@
+import faker
+
+fake = faker.Faker(["en_US", "pl_PL"])
--- corn/models/factories/user_factory.py
+++ corn/models/factories/user_factory.py
@@ -3,23 +3,26 @@ from uuid import UUID
 import factory
 
 from corn.hasher import hasher
+from corn.models.factories.main import fake
 from corn.models.pydantic.user import UserRegistrationPayload
 from corn.models.sqlalchemy.user import User
 
 
-class UserSchemaFactory(factory.Factory):  # type: ignore
+class BaseUserFactory(factory.Factory):  # type: ignore
+    username = fake.user_name()
+    email = fake.ascii_safe_email()
+
+
+class UserSchemaFactory(BaseUserFactory):
     class Meta:
         model = User
 
     id = factory.Sequence(lambda n: UUID(int=n))
-    email = factory.Sequence(lambda n: f"user-{n}@example.com")
-    username = factory.Sequence(lambda n: f"user-{n}@example.com")
     password_hash = hasher.hash("foobar")
 
 
-class UserRegistrationPayloadFactory(factory.Factory):  # type: ignore
+class UserRegistrationPayloadFactory(BaseUserFactory):
     class Meta:
         model = UserRegistrationPayload
-    email = factory.Sequence(lambda n: f"user-{n}@example.com")
-    username = factory.Sequence(lambda n: f"user-{n}@example.com")
+
     password = "foobar"
