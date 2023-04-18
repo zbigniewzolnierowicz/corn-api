34e2b3f28e81892848d6b5d60adbca25acda0883
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@saucelabs.com>
2023-04-18 16:17:44 +0200

    Add Alembic

--- corn/models/sqlalchemy/user.py
+++ corn/models/sqlalchemy/user.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, String
 
 from corn.db import Base
 from corn.hasher import hasher
@@ -9,13 +9,14 @@ from corn.hasher import hasher
 class User(Base):
     __tablename__ = "user_accounts"
 
-    id: Mapped[str] = mapped_column(
+    id: str = Column(
+        String,
         primary_key=True,
         default=lambda: str(uuid.uuid4())
     )
-    username: Mapped[str] = mapped_column(unique=True)
-    password_hash: Mapped[str] = mapped_column()
-    email: Mapped[str] = mapped_column(unique=True)
+    username: str = Column(String, unique=True)
+    password_hash: str = Column(String)
+    email: str = Column(String, unique=True)
 
     def check_password(self, password: str) -> bool:
         return hasher.verify(self.password_hash, password)
